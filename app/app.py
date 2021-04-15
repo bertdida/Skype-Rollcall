c1641f81133f6f73bb341b7f20b68c96a06d4eac
bertdida <38489033+bertdida@users.noreply.github.com>
2021-04-15 08:39:51 +0800

    Listen for new message events

--- app/app.py
+++ app/app.py
@@ -1,11 +1,22 @@
 import os
-from skpy import Skype
+from skpy import SkypeEventLoop, SkypeNewMessageEvent
+
 
 username = os.environ["SKYPE_USERNAME"]
 password = os.environ["SKYPE_PASSWORD"]
 
-sk = Skype(username, password)
 
-contacts = sk.contacts
-for contact in contacts:
-    print(contact)
+class MySkypeEventLoop(SkypeEventLoop):
+    def onEvent(self, event):
+        if not isinstance(event, SkypeNewMessageEvent):
+            return
+
+        if event.msg.userId == self.userId:
+            return
+
+        print(repr(event))
+
+
+if __name__ == "__main__":
+    sk = MySkypeEventLoop(username, password, autoAck=True)
+    sk.loop()
