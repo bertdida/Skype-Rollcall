5ed5db86de66dad18da67ef7e25f834945f3235c
bertdida <38489033+bertdida@users.noreply.github.com>
2021-04-25 11:39:32 +0800

    Add is_admin column to ChannelUser model

--- skyperollcall/models/channeluser.py
+++ skyperollcall/models/channeluser.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, PrimaryKeyConstraint, ForeignKey
+from sqlalchemy import Column, Integer, Boolean, PrimaryKeyConstraint, ForeignKey
+from sqlalchemy.sql.expression import false
 from skyperollcall.models import Base
 from skyperollcall.models.mixins.BaseMixin import BaseMixin
 
@@ -9,3 +10,4 @@ class ChannelUser(Base, BaseMixin):
 
     channel_id = Column(Integer, ForeignKey("channel.id"), nullable=False)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    is_admin = Column(Boolean, server_default=false(), default=False)
