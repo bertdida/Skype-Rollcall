7a0c79312229fa0e57a7af6a7dacc56492af92c2
bertdida <38489033+bertdida@users.noreply.github.com>
2021-04-25 20:42:44 +0800

    Add is_admin method in User model

--- skyperollcall/models/user.py
+++ skyperollcall/models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from skyperollcall.models import Base
+from skyperollcall.models.channeluser import ChannelUser
 from skyperollcall.models.mixins.BaseMixin import BaseMixin
 
 
@@ -11,3 +12,7 @@ class User(Base, BaseMixin):
     skype_id = Column(String, unique=True, nullable=False)
 
     channels = relationship("Channel", secondary="channel_user", back_populates="users")
+
+    def is_admin(self, channel):
+        channel_user = ChannelUser.get(user_id=self.id, channel_id=channel.id)
+        return False if not channel_user else channel_user.is_admin
