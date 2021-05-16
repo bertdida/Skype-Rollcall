f551037d0649c34d656e144d2d5b50a91c202a28
bertdida <38489033+bertdida@users.noreply.github.com>
2021-05-16 13:08:53 +0800

    Fixed postgres database url issue

--- config/__init__.py
+++ config/__init__.py
@@ -3,6 +3,9 @@ import os
 
 class Config:
     DATABASE_URL = os.environ["DATABASE_URL"]
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
     USERNAME = os.environ["SKYPE_USERNAME"]
     PASSWORD = os.environ["SKYPE_PASSWORD"]
     COMMAND_PREFIX = os.environ.get("COMMAND_PREFIX", "!")
